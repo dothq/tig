c882cb191b0762b92f1c6e595d37db1e6f14137a
EnderDev <xenderpugx@gmail.com>
2020-08-07 15:43:34 +0100

    make tig look prettier and check if hg + git is installed

--- bin/tig.js
+++ bin/tig.js
@@ -8,24 +8,31 @@ const http = require('isomorphic-git/http/node')
 const path = require("path");
 const axios = require('axios');
 
+const commandExists = require('command-exists').sync;
+
 const git = require('isomorphic-git')
 const hg = require("hg");
 
 program.version('1.0.0');
 
-const log = (chan, pl) => {
+const log = (chan, pl, upPrev, nl) => {
     const channels = {
         ERROR: chalk.red.bold,
         WARN: chalk.yellow.bold,
         INFO: chalk.blue.bold,
-        SUCCESS: chalk.green.bold
+        SUCCESS: chalk.green.bold,
+        PROCESS: chalk.magenta.bold,
+        HELPER: chalk.cyan.bold
     }
 
-    console.log(`${channels[chan](chan)}`, pl)
+    if(upPrev) {
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      process.stdout.write(`${channels[chan](chan)} ` + pl);
+    } else {
+      console.log(`${nl ? "\n" : ""}${channels[chan](chan)}`, pl) 
+    }
 }
-
-const gitRegex = /[a-z]+\/[a-z]+/;
-const hostRegex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$|^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$/;
 
 const fancyTime = (duration) => {
   var milliseconds = parseInt((duration % 1000) / 100),
@@ -44,10 +51,40 @@ program
   .command('fetch <hgProject> <gitProject>')
   .description('fetch a project')
   .action((hgProject, gitProject, sw) => {
+    const t = Date.now();
+
+    log("INFO", `Checking if Mercurial is installed on your system...`, true)
+    const mercurialInstalled = commandExists("hg");
+
+    log("INFO", `Checking if Git is installed on your system...`, true)
+    const gitInstalled = commandExists("git");
+
+    if(!mercurialInstalled) {
+      log("ERROR", `For \`tig\` to work, you will need to install Mercurial.`)
+
+      var platform = require("os").platform
+
+      const hash = platform == "win32" ? "#Windows" : platform == "darwin" ? "#Mac_OS_X" : ""
+
+      return log("ERROR", `Mercurial can be installed for your \`${require("os").platform}\` machine by following the instructions at https://www.mercurial-scm.org/wiki/Download${hash}.`)
+    }
+
+    if(!gitInstalled) {
+      log("ERROR", `For \`tig\` to work, you will need to install Git.`)
+
+      const platform = require("os").platform
+
+      const path = platform == "win32" ? "win" : platform == "darwin" ? "mac" : "linux"
+
+      return log("ERROR", `Git can be installed for your \`${require("os").platform}\` machine by downloading Git at https://git-scm.com/download/${path}.`)
+    }
+
     if(!hgProject.startsWith("http") || !hgProject.startsWith("https")) hgProject = `https://` + hgProject;
 
     const repo = hgProject.split("://")[1].split("/");
     repo.shift()
+
+    log("HELPER", `Preparing \`${repo.join("/")}\` for mercurial clone at server \`${hgProject.split("://")[1].split("/").shift()}\`.`, false, true)
 
     const projectName = repo.join("/").split("/").pop();
 
@@ -56,33 +93,32 @@ program
     const gitRepo = gitProject.split("://")[1].split("/");
     gitRepo.shift()
 
-    log("INFO", `Fetching \`${repo.join("/")}\` information from mercurial host \`${hgProject.split("://")[1].split("/").shift()}\`.`)
-
     const dir = path.join(process.cwd(), projectName)
 
-    log("INFO", `Starting clone of mercurial repository \`${repo.join("/")}\` located at \`${hgProject.split("://")[1].split("/").shift()}\`, this may take a while...`)
-
-    const t = Date.now();
+    if(!done) {
+      log("PROCESS", `Starting clone of mercurial repository \`${repo.join("/")}\` located at \`${hgProject.split("://")[1].split("/").shift()}\`, this may take a while...`, false, false)
+    }
 
     var int = setInterval(() => {
-      console.log("\n")
-      log("INFO", `[${fancyTime(Date.now() - t)}] Tig is still cloning...\n`)
-    }, 5000)
+      log("INFO", `[${fancyTime(Date.now() - t)}] Tig is still cloning...`, true)
+    }, 1000)
 
     var failed = false;
+    var done = false;
 
-    hg.clone(hgProject, dir, (e, out) => {
+    hg.clone(hgProject, dir, (e, out) => {      
       if(e) {
+        done = true;
         failed = true;
 
-        log("ERROR", e.message)
-        console.log(e.stack.replace("Error: " + e.message, ""), "\n")
+        log("ERROR", e.message, false, true)
 
         return clearInterval(int)
       }
 
       if(!failed && out) {
-        log("SUCCESS", `Cloned mercurial repository \`${repo.join("/")}\` in \`${fancyTime(Date.now() - t)}s\``)
+        done = true;
+        log("SUCCESS", `Cloned mercurial repository \`${repo.join("/")}\` in \`${fancyTime(Date.now() - t)}s\``, true, true)
         return clearInterval(int)
       }
     });
